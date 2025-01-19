ef8c071f8a83cbe55eab25d18bde2459c8e56e3c
irinaguseva <ireneguseva9@gmail.com>
2025-01-19 22:06:15 +0300

    added changes for docker

--- team_management/serializers.py
+++ team_management/serializers.py
@@ -6,29 +6,15 @@ class CreatorSerializer(serializers.ModelSerializer):
         model = Creator
         fields = '__all__'
 
-    #def validate_money(self, value):
-        #if value < 0:
-            #raise serializers.ValidationError("Money cannot be negative.")
-
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
         fields = '__all__'
 
-    #def validate_name(self, value):
-        #if Team.objects.filter(name=value).exists():
-            #raise serializers.ValidationError("Team with this name already exists.")
-        #return value
-
 class MemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Member
         fields = '__all__'
-
-    #def validate_stamina(self, value):
-        #if value < 0:
-            #raise serializers.ValidationError("Stamina cannot be negative.")
-        #return value
 
 class TeamApplicationSerializer(serializers.ModelSerializer):
     class Meta:
