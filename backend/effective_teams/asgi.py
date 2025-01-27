eac8e2cdf313da5f952b47613e7fca2c8af78ff4
irinaguseva <ireneguseva9@gmail.com>
2025-01-27 22:08:51 +0300

    changes for names and env

--- backend/effective_teams/asgi.py
+++ backend/effective_teams/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for effective_team project.
+ASGI config for effective_teams project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'effective_team.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'effective_teams.settings')
 
 application = get_asgi_application()
--- backend/effective_teams/wsgi.py
+++ backend/effective_teams/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for effective_team project.
+WSGI config for effective_teams project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'effective_team.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'effective_teams.settings')
 
 application = get_wsgi_application()
